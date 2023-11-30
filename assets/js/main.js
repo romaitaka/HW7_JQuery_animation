2fe83ac6c57553eaa11b688bdfa14d388a820103
romaitaka <trade.interes@gmail.com>
2023-11-30 20:25:31 +0300

    Jquery animation v1
    Parallax and tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,3 +57,29 @@ $(document).ready(function () {
     }
   });
 });
+
+
+//Slider
+
+$(document).ready(function () {
+  var swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+
+    // If you need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
